00fb0abe0efa6709dbc256d0cd588318cabae88c
ashrafbd1496 <asraf1496@gmail.com>
2025-04-22 12:48:32 +0600

    used defineProps with type check

--- component-and-node/src/components/User/Profile.vue
+++ component-and-node/src/components/User/Profile.vue
@@ -2,13 +2,22 @@
   <div class="user_profile">
     <h1>User Information</h1>
     <ul>
-      <li><span>Name: </span> {{name}}</li>
-      <li><span>Age: </span> {{age}}</li>
+      <li><span>Name: </span> {{ name }}</li>
+      <li><span>Age: </span> {{ age }}</li>
     </ul>
   </div>
 </template>
 <script setup>
-const props = defineProps(['name', 'age']);
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+});
 </script>
 <style scoped>
 li > span {
@@ -27,5 +36,5 @@ h1 {
 ul {
   list-style-type: none;
   padding: 0;
-} 
+}
 </style>
